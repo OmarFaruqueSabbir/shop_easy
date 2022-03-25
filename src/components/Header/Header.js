e70bb7a0e6baf06bff33cb16e4a0daae40075d6f
OmarFaruqueSabbir <avrosabbir18@gmail.com>
2022-03-25 22:08:04 +0600

    Header Added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../images/SVG/Asset 1.svg'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='header'>
+                <img src={logo} alt="" />
+                <div>
+                    <a href="/Shop">Shop</a>
+                    <a href="/Orders">Orders</a>
+                    <a href="/Orders">About</a>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
